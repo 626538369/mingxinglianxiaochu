8a06a66fdff608a2f6a42d211db6df186b9c17ac
luhaitao <luhaitao@h3d.com.cn>
2018-07-12 16:58:28 +0800

    第一波提交

---
+++ Code/Assets/Plugins/sdk/game-api/global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+static public class global
+{
+    public const string cp_adress = "ATEETtk8gYUokA9kCuuRDAXvH3VV94ewMc";   //公司充值账户
+    public const string api = "https://api.nel.group/api/testnet";
+    public const string id_GAS = "0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7"; ///对应neogas
+    //static public string id_sgas = "0x3f7420285874867c30f32e44f304fd62ad1e9573";
+    static public string id_sgas = "0x2761020e5e6dfcd8d37fdd50ff98fa0f93bccf54";///nep5资产，一种资产类型。gui要显示的话，高级->选项里面把sgas的id填进去
+
+    static public int game_id = 4;
+}
